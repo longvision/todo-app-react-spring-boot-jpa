fcad3133ffdadbdf02a7b785ab250325da485a2d
Ricardo <r.n.hori@gmail.com>
2022-04-17 14:19:54 -0700

    Docker containerization: server + postgres image generation

--- frontend/src/__fake-api__/api.ts
+++ frontend/src/__fake-api__/api.ts
@@ -3,4 +3,4 @@ import axios from "axios";
 export const api = axios.create({ baseURL: "http://localhost:8080" });
 
 api.defaults.headers.common["Content-Type"] = "application/json";
-// //console.log(`ENV: ${configs.api}`);
+
